0d7844408595cb0ede54c0bbb0682a52d27eb68a
Joshua-Adler <signup1579@gmail.com>
2021-05-31 03:18:02 -0700

    Fixed the camera bug

--- Assets/Scripts/Camera/PlayerTracking.cs
+++ Assets/Scripts/Camera/PlayerTracking.cs
@@ -2,24 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerTracking : MonoBehaviour
-{
+public class PlayerTracking : MonoBehaviour {
+
 	public float closingRatio = 0.9F;
 	float logClosingRatio;
 
 	GameObject player;
 
 	// Start is called before the first frame update
-	void Start()
-	{
+	void Start() {
 		player = GameObject.Find("Player");
 		logClosingRatio = -1 * Mathf.Log(1 - closingRatio);
 	}
 
 	// Update is called once per frame
-	void Update()
-	{
-		Vector3 deltaPosition = player.transform.position - gameObject.transform.position;
-		transform.position += logClosingRatio * deltaPosition * Time.deltaTime;
+	void LateUpdate() {
+		Vector2 deltaPosition = player.transform.position - gameObject.transform.position;
+		transform.position += (Vector3) (logClosingRatio * deltaPosition * Time.deltaTime);
 	}
 }
