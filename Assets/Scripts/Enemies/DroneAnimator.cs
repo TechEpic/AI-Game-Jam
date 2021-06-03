698b2446c6661a5c731b743f6db4b8a543cab866
Joshua-Adler <signup1579@gmail.com>
2021-06-03 02:36:31 -0700

    a

--- Assets/Scripts/Enemies/DroneAnimator.cs
+++ Assets/Scripts/Enemies/DroneAnimator.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class DroneAnimator : MonoBehaviour {
 	
-	static Sprite[] stationary;
+	public float Speed = 20;
+
+	static readonly int IDLE = 1;
+	static readonly int SIGNAL = 2;
+	static readonly int WALK = 3;
+	static int dirOffset = 100;
+	static int[] stateOffsets = {0, 0, 30, 60, dirOffset};
+	static Sprite[] sprites;
+
+	int curState;
+	float animIndex;
 	SpriteRenderer sr;
+	Rigidbody2D rb;
 
 	void Start() {
 		sr = gameObject.GetComponent<SpriteRenderer>();
-		
-		if(stationary == null) {
-			stationary = Resources.LoadAll<Sprite>("Drone");
+		rb = gameObject.GetComponent<Rigidbody2D>();
+		if(sprites == null) {
+			sprites = Resources.LoadAll<Sprite>("Drone");
 		}
+		animIndex = 0;
+		curState = IDLE;
 	}
 
-	int fc = 0;
+
 	void Update() {
-		sr.sprite = stationary[fc++ % stationary.Length];
+		animIndex += Time.deltaTime * Speed;
+		animIndex %= stateOffsets[curState + 1] - stateOffsets[curState];
+
+		int index = (int) (animIndex + (int) ((Mathf.Atan2(rb.velocity.y, rb.velocity.x) + Mathf.PI) / Mathf.PI * 4) * dirOffset);
+
 	}
 }
